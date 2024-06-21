dd090a5a856b7b8077cd1ab23810109f2a684510
Anael DELANNOY <anaeldelannoy@gmail.com>
2024-06-21 23:06:38 +0200

    Update EtatDesLieuxDAO

--- src/main/java/org/example/my_expert_inventory/dao/EtatDesLieuxDAO.java
+++ src/main/java/org/example/my_expert_inventory/dao/EtatDesLieuxDAO.java
@@ -1,4 +1,37 @@
 package org.example.my_expert_inventory.dao;
 
+import org.example.my_expert_inventory.model.EtatDesLieux;
+import jakarta.persistence.EntityManager;
+
 public class EtatDesLieuxDAO {
+    private EntityManager entityManager;
+
+    public EtatDesLieuxDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public EtatDesLieux findById(int id) {
+        return entityManager.find(EtatDesLieux.class, id);
+    }
+
+    public EtatDesLieux create(EtatDesLieux etatDesLieux) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(etatDesLieux);
+        entityManager.getTransaction().commit();
+        return etatDesLieux;
+    }
+
+    public EtatDesLieux update(EtatDesLieux etatDesLieux) {
+        entityManager.getTransaction().begin();
+        EtatDesLieux updatedEtatDesLieux = entityManager.merge(etatDesLieux);
+        entityManager.getTransaction().commit();
+        return updatedEtatDesLieux;
+    }
+
+    public void delete(int id) {
+        EtatDesLieux etatDesLieux = findById(id);
+        entityManager.getTransaction().begin();
+        entityManager.remove(entityManager.contains(etatDesLieux) ? etatDesLieux : entityManager.merge(etatDesLieux));
+        entityManager.getTransaction().commit();
+    }
 }
