c3f297fb81afbaf5f3e916ff49bd00cb2cea1e25
Anael DELANNOY <anaeldelannoy@gmail.com>
2024-06-21 23:05:08 +0200

    Update ElementDAO

--- src/main/java/org/example/my_expert_inventory/dao/ElementDAO.java
+++ src/main/java/org/example/my_expert_inventory/dao/ElementDAO.java
@@ -1,4 +1,37 @@
 package org.example.my_expert_inventory.dao;
 
+import org.example.my_expert_inventory.model.Element;
+import jakarta.persistence.EntityManager;
+
 public class ElementDAO {
+    private EntityManager entityManager;
+
+    public ElementDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public Element findById(Long id) {
+        return entityManager.find(Element.class, id);
+    }
+
+    public Element create(Element element) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(element);
+        entityManager.getTransaction().commit();
+        return element;
+    }
+
+    public Element update(Element element) {
+        entityManager.getTransaction().begin();
+        Element updatedElement = entityManager.merge(element);
+        entityManager.getTransaction().commit();
+        return updatedElement;
+    }
+
+    public void delete(Long id) {
+        Element element = findById(id);
+        entityManager.getTransaction().begin();
+        entityManager.remove(entityManager.contains(element) ? element : entityManager.merge(element));
+        entityManager.getTransaction().commit();
+    }
 }
