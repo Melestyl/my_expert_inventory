d543325be5205632698734234da3a3b8305ed80f
Anael DELANNOY <anaeldelannoy@gmail.com>
2024-06-21 22:58:20 +0200

    Update AdresseDAO

--- src/main/java/org/example/my_expert_inventory/dao/AdresseDAO.java
+++ src/main/java/org/example/my_expert_inventory/dao/AdresseDAO.java
@@ -1,4 +1,37 @@
 package org.example.my_expert_inventory.dao;
 
+import org.example.my_expert_inventory.model.Adresse;
+import jakarta.persistence.EntityManager;
+
 public class AdresseDAO {
+    private EntityManager entityManager;
+
+    public AdresseDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public Adresse findById(int id) {
+        return entityManager.find(Adresse.class, id);
+    }
+
+    public Adresse create(Adresse adresse) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(adresse);
+        entityManager.getTransaction().commit();
+        return adresse;
+    }
+
+    public Adresse update(Adresse adresse) {
+        entityManager.getTransaction().begin();
+        Adresse updatedAdresse = entityManager.merge(adresse);
+        entityManager.getTransaction().commit();
+        return updatedAdresse;
+    }
+
+    public void delete(int id) {
+        Adresse adresse = findById(id);
+        entityManager.getTransaction().begin();
+        entityManager.remove(entityManager.contains(adresse) ? adresse : entityManager.merge(adresse));
+        entityManager.getTransaction().commit();
+    }
 }
