c2625eb4bc2e45985aed60a80ccc40567ef84473
Anael DELANNOY <anaeldelannoy@gmail.com>
2024-06-24 20:51:32 +0200

    Add view-inventory-select-bien.fxml And update Home.java

--- src/main/java/org/example/my_expert_inventory/controllers/Home.java
+++ src/main/java/org/example/my_expert_inventory/controllers/Home.java
@@ -33,20 +33,27 @@ public class Home implements Initializable {
 	void setViewInventory() throws IOException {
 		System.out.println("View Inventory");
 		try {
-			SceneManager.loadScene("create-inventory-select-bien.fxml", createInventory);
+			SceneManager.loadScene("create-inventory-select-bien.fxml", viewInventory);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	@FXML
 	void setCreateInventory() {
 		System.out.println("Create Inventory");
+		{
+			System.out.println("View Inventory");
+			try {
+				SceneManager.loadScene("create-inventory-select-bien.fxml", createInventory);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
-		System.out.println("Home");
+
 	}
 }
