d837e7c6b28c04b431837dadf2e184615d018f93
Melestyl <tom.telcal@icloud.com>
2024-06-24 21:31:37 +0200

    View is created and a table is empty

--- src/main/java/org/example/my_expert_inventory/controllers/CreateInventory.java
+++ src/main/java/org/example/my_expert_inventory/controllers/CreateInventory.java
@@ -2,6 +2,7 @@ package org.example.my_expert_inventory.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import org.example.my_expert_inventory.model.Bien;
 
 import java.net.URL;
@@ -10,9 +11,21 @@ import java.util.ResourceBundle;
 public class CreateInventory implements Initializable {
 	public static Bien bien;
 
+	@FXML
+	public Button backToHome;
+
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
 		System.out.println("Create Inventory");
 		System.out.println(bien);
+	}
+
+	@FXML
+	public void setBackToHome() {
+		try {
+			SceneManager.loadScene("home.fxml", backToHome);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
