c1c950eb144deab888f21c1b63e4620a1ded14e1
Anael DELANNOY <anaeldelannoy@gmail.com>
2024-06-25 23:08:48 +0200

    Structure

--- src/main/java/org/example/my_expert_inventory/controllers/AddElement.java
+++ src/main/java/org/example/my_expert_inventory/controllers/AddElement.java
@@ -1,4 +1,46 @@
 package org.example.my_expert_inventory.controllers;
 
-public class AddElement {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import org.example.my_expert_inventory.service.BienService;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class AddElement implements Initializable {
+
+    @FXML
+    private GridPane table;
+
+    @FXML
+    private Button addRow;
+
+    @FXML
+    private Button saveElements;
+
+    @FXML
+    private Button removeLastRow;
+
+    void setAddRow() {
+        // Add a new row to the table
+    }
+
+    void setSaveElements() {
+        // Save the elements in the table
+    }
+
+    void setRemoveLastRow() {
+        // Remove the last row from the table
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
